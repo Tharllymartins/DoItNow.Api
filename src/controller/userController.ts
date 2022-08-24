338d707046b9355703a7ad1c2892c61c42aaa687
Tharlly Alves Martins <tharllymartins16@gmail.com>
2022-08-24 13:42:09 -0300

    added controller to reset password

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,28 +4,38 @@ import User from "../models/User";
 import AuthUserService from "../services/AuthUserService";
 import CreateUserService from "../services/CreateUserService";
 import UpdatedUserAvatarService from "../services/UpdatedUserAvatarService";
+import { hash } from "bcryptjs";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+import ForgotUserPassword from "../services/ForgotUserPasswordService";
 
 
 const getUserController = async (req: Request, res: Response) => {
-    const id = req.user.id;
-    const userRepo = getRepository(User)
-    const user = await userRepo.findOne({
-        select: ["name", "email", "avatar"],
-        where: {
-            id
-        }
-    }) as User
 
-    if(!user){
-        return res.status(400).json()
-    }
+    try {
+        const id = req.user.id;
+        const userRepo = getRepository(User)
+        const user = await userRepo.findOne({
+            select: ["name", "email", "avatar"],
+            where: {
+                id
+            }
+        }) as User
     
-    return res.json(user)
+        if(!user){
+            return res.status(400).json()
+        }
+        
+        return res.json(user)
+    } catch (error: Error | any) {
+        return res.status(400).json({error: error.message})
+    }
+
 }
 
 const authUserController = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
     try {
+        const { email, password } = req.body;
         const authUser = new AuthUserService;
         const  user  = await authUser.execute({
             email,
@@ -41,9 +51,9 @@ const authUserController = async (req: Request, res: Response) => {
 }
 
 const createUserController = async (req: Request, res: Response) => {
-    const { email, name, password } = req.body;
-    const createUser = new CreateUserService;
     try {
+        const { email, name, password } = req.body;
+        const createUser = new CreateUserService;
         await createUser.execute({
             email,
             name,
@@ -71,15 +81,27 @@ const uploadUserAvatarController = async (req: Request, res: Response) => {
         delete user.password;
 
         return res.json(user)
-    } catch (error) {
-        return res.json({error: error})
+    } catch (error: Error | any) {
+        return res.json({error: error.message})
     }
 }
 
+const forgotUserPasswordController = async (req: Request, res: Response) => {
+    try {
+        const { email } = req.body;
+        const forgotUserPasswordService = new ForgotUserPassword()
+        const passwordChanged = await forgotUserPasswordService.execute({email})
+
+        return res.send("success!")
+    } catch (error: Error | any) {
+        return res.status(400).json({error: error.message})
+    }
+}
 
 export {
     getUserController,
     authUserController,
     createUserController,
-    uploadUserAvatarController
+    uploadUserAvatarController,
+    forgotUserPasswordController
 }
