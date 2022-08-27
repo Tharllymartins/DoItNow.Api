945df43e406c2b3a8914fb3c009cf6d121c9cf5e
Tharlly Alves Martins <tharllymartins16@gmail.com>
2022-08-27 11:27:10 -0300

    added route to change user password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import ensureAutheticated from "../middlewares/ensureAutheticated";
 import multer from "multer";
 import uploadConfig from "../config/upload";
-import { authUserController, createUserController, forgotUserPasswordController, getUserController, uploadUserAvatarController } from "../controller/userController";
+import { authUserController, changePasswordController, createUserController, forgotUserPasswordController, getUserController, uploadUserAvatarController } from "../controller/userController";
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -17,6 +17,8 @@ usersRouter.get("/auth/me", ensureAutheticated, getUserController)
 usersRouter.patch('/avatar', ensureAutheticated, upload.single('avatar'), uploadUserAvatarController)
 
 usersRouter.post("/forgot-password", forgotUserPasswordController)
+
+usersRouter.post("/change-password", changePasswordController)
 
 
 export default usersRouter;
